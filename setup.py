e675bdcd4da32f5481ede75473c272984ba6b0e9
jim-bo <jlindsay@jimmy.harvard.edu>
2015-12-22 13:13:43 -0500

    added requirements

--- setup.py
+++ setup.py
@@ -6,4 +6,5 @@ setup(name='oncotreenx',
       author_email="jlindsay@jimmy.harvard.edu",
       url="https://github.com/jim-bo/oncotreenx",
       py_modules=['oncotreenx'],
+      install_requires = ['networkx', 'nose'],
       )
